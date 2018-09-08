b4a6d21ea87eb929598ecac7edc61ef7b087a910
Steven Crouse <sdcrouse91@gmail.com>
2018-09-08 01:43:40 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project 
+  attr_reader :title, :backers 
+  # Personally, I don't think that :backers belongs in attr_accessor, as it is an array.
+  # It doesn't make sense to me to have @backers include a writer method through attr_accessor.
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end 
+  
+  def add_backer(backer)
+    self.backers << backer
+    backer.back_project(self)
+  end
+  
+end
